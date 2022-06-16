690730e4521fe5418fed70f8ca0f43d549d76df8
Danil Nikitin <d.nikitin.dev@gmail.com>
2022-06-16 14:46:21 +0300

    Add parse book links from category.

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -6,18 +6,22 @@ from bs4 import BeautifulSoup
 from settings import Settings
 
 
-def parse_book_url_for_download() -> None:
-    """Parse book link from book page for download."""
+def parse_book_links_for_download() -> None:
+    """Parse book links from book cards for download."""
     settings = Settings()
 
     book_page = requests.get(url=urljoin(settings.SITE_URL_ROOT, "l55"))
     book_page.raise_for_status()
 
     soup = BeautifulSoup(book_page.text, "lxml")
-    book_uri = soup.find("table", class_="d_book").find("a")["href"]
-    book_url = urljoin(settings.SITE_URL_ROOT, book_uri)
-    print(f"{book_url=}")
+    book_cards = soup.find(id="content").find_all("table", class_="d_book")
+    book_links = [
+        book_card.find_next("a")["href"] for book_card in book_cards
+    ]
+
+    for book_link in book_links:
+        print(urljoin(settings.SITE_URL_ROOT, book_link))
 
 
 if __name__ == "__main__":
-    parse_book_url_for_download()
+    parse_book_links_for_download()
